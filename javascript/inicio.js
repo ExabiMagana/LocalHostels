77a98532472653c7ab9883a2cbfddf3f3ecbb38b
ExabiMagana <maganaexabi@gmail.com>
2025-05-12 02:45:44 -0600

    Versión 1

--- javascript/inicio.js
+++ javascript/inicio.js
@@ -1,4 +1,4 @@
-const paginas = ["santaAna.html","sonsonate.html","ahuachapan.html"];
+const paginas = ["La_Casa_Del_Pinito.html","Lali_Beach_Hotel_Boutique.html","Marsella.html"];
 
 // Función para abrir una página aleatoria
 function paginaAleatoria() {
@@ -27,4 +27,51 @@ document.addEventListener('click', function () {
     document.querySelectorAll('.flip-card.active').forEach(card => {
         card.classList.remove('active');
     });
+});
+
+
+//Para la búsqueda
+const hospedajes = {
+    "la casa del pinito": "La_Casa_Del_Pinito.html",
+    "lali beach hotel boutique": "Lali_Beach_Hotel_Boutique.html",
+    "marsella": "Marsella.html",
+    "santa ana": "CiudadSantaAna.html",
+    "sonsonate": "CiudadSonsonate.html",
+    "ahuachapán": "CiudadAhuachapan.html",
+    "ahuachapan": "CiudadAhuachapan.html"
+};
+
+function buscarHospedaje() {
+    const input = document.getElementById("busqueda").value.trim().toLowerCase();
+
+    if (input === "") {
+        alert("Por favor, escribe el nombre de un hospedaje para buscar.");
+        return;
+    }
+    if (input.length < 2) {
+        alert("Por favor, escribe al menos 2 caracteres para la búsqueda.");
+        return;
+    }
+
+    let encontrado = null;
+    for (const nombre in hospedajes) {
+        if (nombre.includes(input)) {
+            encontrado = hospedajes[nombre];
+            break;
+        }
+    }
+
+    if (encontrado) {
+       setTimeout(function() {
+            window.location.href = encontrado;
+        }, 1000);
+    } else {
+        alert("No se encontraron coincidencias.");
+    }
+}
+
+document.getElementById("busqueda").addEventListener("keydown", function(e) {
+    if (e.key === "Enter") {
+        buscarHospedaje();
+    }
 });
